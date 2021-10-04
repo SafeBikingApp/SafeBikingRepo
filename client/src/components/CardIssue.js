7c089a7bda00f485d92574f8365325149f61c0d4
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-10-04 18:08:22 +0200

    Corrections

--- client/src/components/CardIssue.js
+++ client/src/components/CardIssue.js
@@ -1,10 +1,15 @@
 import React, {useState} from "react";
+import { Link } from "react-router-dom";
 import "./CSS/CardIssue.css";
+import Votes from "./Votes";
 
-function CardIssue() {
+function CardIssue(props) {
 
     return (
-        <div className="cardissue-wrapper dark-color-bg">
+        <div className="cardissue-wrapper light-color-bg">
+            <div className="cardissue-title dark-color-bg">{props.title}</div>
+            <div className=""><Link to={"_"}><Votes /></Link></div>
+            <div className="cardissue-title dark-color-bg"><Link to={"_"}>More info</Link></div>
         </div>
     )
 };
--- client/src/components/CardReportIssue.js
+++ client/src/components/CardReportIssue.js
@@ -5,11 +5,8 @@ import "./CSS/CardReportIssue.css";
 function CardReportIssue(props) {
 
     return (
-        <div className="cardreportissue-wrapper light-color-bg">
-            <div className="dark-color-bg">{props.title}</div>
-            <div className=""><Link to={"_"}></Link></div>
-            <div className="dark-color-bg"><Link to={"_"}>More info</Link></div>
-            
+        <div className="cardreportissue-wrapper dark-color-bg">
+            <Link to="">Report Issue</Link>
         </div>
     )
 };
