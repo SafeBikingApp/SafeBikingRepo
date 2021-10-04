fc4d2d9b9aa58bda583becf6b869c93aa2f3ebaa
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-10-04 23:38:20 +0200

    ReportIssue component build

--- client/src/components/ReportIssue.js
+++ client/src/components/ReportIssue.js
@@ -1,28 +1,65 @@
 import React, {useState} from "react";
-import "./CSS/Header.css";
+import "./CSS/ReportIssue.css";
 import Title from "./Title";
 import Button from "./Button";
 
 function ReportIssue() {
 
-    const handleClick = () => {
-
+    const handleClick = (e) => {
+        setIssue(e.target.value)
     };
+    const handleClickPhoto = (e) => {
+        
+    };
+    const [issue, setIssue] = useState("")
+    const [agree, setAgree] = useState(false);
+    const [photo, setPhoto] = useState(true);
+    const [imgUrl, setImgUrl] = useState("");
 
     return (
         <div className="reportissue-wrapper">
             <Title title="Report Issue" />
             <div className="reportissue-section-container">
-                <div className="reportissue-title">
+                <div className="reportissue-title dark-color-text">
                     Issue:
                 </div>
                 <div className="reportissue-detail">
-                    <Button className="" component="" handleClick={handleClick} name="OBSTRUCTION" />
+                    <Button component="" handleClick={handleClick} name="OBSTRUCTION" />
                     <Button component="" handleClick={handleClick} name="POTHOLE" />
+                </div>
                 <div className="reportissue-detail">
                     <Button component="" handleClick={handleClick} name="ACCIDENT" />
                     <Button component="" handleClick={handleClick} name="TRAFFIC DANGER" />
                 </div>
+            </div>
+            <div className="reportissue-section-container dark-color-text">
+                <div className="reportissue-title dark-color-text">
+                    Description:
+                </div>
+                <div className="reportissue-detail reportissue-left-input">
+                    <textarea className="reportissue-input" rows="3"/>
+                </div>
+            </div>
+            <div className="reportissue-section-container">
+                <div className="reportissue-title dark-color-text">
+                    Photo:
+                </div>
+                <div className="reportissue-detail reportissue-left-input">
+                    <input className="reportissue-input" placeholder="optional" type="text" />
+                    &nbsp;&nbsp;&nbsp;
+                    <Button name="UPLOAD" onClick={handleClickPhoto} />
+                </div>
+                <div className={photo ? "reportissue-detail reportissue-center reportissue-small" : "reportissue-detail hide-photo"}>
+                    <img src={imgUrl} alt="Evidence" className="reportissue-photo light-color-bg" />
+                </div>
+                <div className="reportissue-detail reportissue-small dark-color-text">
+                    Photo preview
+                </div>
+                <div className="reportissue-detail dark-color-text reportissue-left">
+                    <input type="checkbox" className="reportissue-checkbox" onClick={setAgree} />I agree to the terms of service.
+                </div>
+                <div className="reportissue-center">
+                    <Button name="REPORT" component="" onClick={handleClick} />
                 </div>
             </div>
         </div>
--- client/src/components/UserLogin.js
+++ client/src/components/UserLogin.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import "./CSS/UserLogin.css";
+import Title from "./Title";
 
 function UserLogin() {
 
     return (
         <div className="userlogin-wrapper">
-
+            <Title title="User LogIn" />
+            
         </div>
     )
 };
