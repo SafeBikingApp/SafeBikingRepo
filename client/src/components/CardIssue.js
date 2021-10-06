95332838f2bb2d154cd2e73cdf3983f741cd0251
Salvo <salvopatti110@gmail.com>
2021-10-06 05:45:26 +0200

    adding  onclick func on both bike icon and card

--- client/src/components/CardIssue.js
+++ client/src/components/CardIssue.js
@@ -6,7 +6,10 @@ import Votes from "./Votes";
 function CardIssue(props) {
   return (
     <div className="cardissue-wrapper light-color-bg">
-      <div className="cardissue-title dark-color-bg">{props.title}</div>
+      <div className="cardissue-title dark-color-bg">
+        {props.title}
+        <span onClick={() => props.setShowIcon(true)}>&#10006;</span>
+      </div>
       <div className="cardissue-body">
         <Votes color={false} issue_id={props.issue_id} />
       </div>
